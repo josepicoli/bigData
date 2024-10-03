50b62ad4a1a3a46d6ffb2bd21464a2ec7fd35bfb
UsernameJonatasIsNotAvailable <contatopessoaljonatas@hotmail.com>
2024-10-03 14:04:36 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+tabela = pd.read_csv("acidentes2024.csv", sep=";", encoding="Windows-1252")
+
+estadoDesejado = "PI"
+tabelaFiltrada = tabela[tabela["uf"] == estadoDesejado]
+print(tabelaFiltrada)
+
+plt.hist(tabelaFiltrada["idade"], bins=20)
+plt.title("Distribuição de idades em acidentes no PI")
+plt.xlabel("idade")
+plt.ylabel("Frequência")
+plt.show()
